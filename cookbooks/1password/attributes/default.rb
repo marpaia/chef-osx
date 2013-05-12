1a4f46793f5520839f8dad60581561499f5f4bd1
Mike Arpaia <mike@arpaia.co>
2013-05-12 17:28:28 -0400

    Initial commit

---
+++ cookbooks/1password/attributes/default.rb
@@ -0,0 +1,2 @@
+default['one_password']['version'] = '3.8.21'
+default['one_password']['checksum'] = "f8dd3b0b4acf6474ac9ba39b8f3fd2f98961064fb266243a62ceb59eb564c889"
---
+++ cookbooks/googlechrome/attributes/default.rb
@@ -0,0 +1,2 @@
+# SHA256 as of 5/12/13
+default['googlechrome']['checksum'] = "206de9c93cd63901bd21d1c018df7fbb96d1081730f06a484bc75b790d40ca38"
