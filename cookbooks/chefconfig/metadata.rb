97e45e337b34d1b25630964a4b309eff4250b552
Mike Arpaia <mike@arpaia.co>
2013-05-12 18:13:30 -0400

    chefconfig cookbook

---
+++ cookbooks/chefconfig/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Mike Arpaia"
+maintainer_email "mike@arpaia.co"
+license          "Apache 2.0"
+description      "Sets up Chef configuration files"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
+supports         "mac_os_x"
