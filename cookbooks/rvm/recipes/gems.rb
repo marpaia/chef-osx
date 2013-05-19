24d624f5fd0bea5759d7110e704f7762c6c07f66
Mike Arpaia <mike@arpaia.co>
2013-05-19 14:34:27 -0400

    adding sinatra to the rvm cookbook

--- cookbooks/rvm/recipes/gems.rb
+++ cookbooks/rvm/recipes/gems.rb
@@ -2,3 +2,8 @@ execute "install chef gem" do
   command "gem install chef"
   not_if "gem list | grep chef"
 end
+
+execute "install sinatra gem" do
+  command "gem install sinatra"
+  not_id "gem list | grep sinatra"
+end
