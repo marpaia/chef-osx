b7ef547315fbad7b31ced4cc16677309a1139a8a
Mike Arpaia <mike@arpaia.co>
2013-05-19 04:15:50 -0400

    close #17

--- cookbooks/mac_os_x/recipes/security.rb
+++ cookbooks/mac_os_x/recipes/security.rb
@@ -29,3 +29,15 @@ mac_os_x_userdefaults "enable OS X firewall" do
   type "int"
   sudo true
 end
+
+execute "disable guest login AppleFileServer" do
+  command "sudo defaults write /Library/Preferences/com.apple.AppleFileServer guestAccess 0"
+  not_if "defaults read /Library/Preferences/com.apple.AppleFileServer guestAccess | grep 0"
+end
+
+
+execute "disable guest login smb server" do
+  command "sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server AllowGuestAccess 0"
+  not_if "defaults read /Library/Preferences/SystemConfiguration/com.apple.smb.server AllowGuestAccess | grep 0"
+end
+
