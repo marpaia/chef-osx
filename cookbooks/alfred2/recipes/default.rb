fec1af056d31c459b7c1d061e9a8d3607ef22f7e
Mike Arpaia <mike@arpaia.co>
2013-05-18 21:57:14 -0400

    most of #28

--- cookbooks/alfred2/recipes/default.rb
+++ cookbooks/alfred2/recipes/default.rb
@@ -1,3 +1,20 @@
+unless ::File.directory?("/Applications/Alfred.app")
+  remote_file "#{Chef::Config[:file_cache_path]}/Alfred_2.0.3_187.zip" do
+    source "http://cachefly.alfredapp.com/Alfred_2.0.3_187.zip"
+    checksum "d49b747ef4a2b49f401501f813f4cb345b899c691202f998233a03fea06bc0a9"
+  end
+
+  execute "unzip alfred" do
+    command "unzip #{Chef::Config[:file_cache_path]}/Alfred_2.0.3_187.zip" 
+    cwd "/tmp"
+  end
+
+  execute "install alfred" do
+    command "mv /tmp/Alfred\\ 2.app /Applications"
+  end 
+
+end
+
 cookbook_file "#{ENV['HOME']}/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist" do
   source "com.runningwithcrayons.Alfred-Preferences.plist"
   only_if { ::File.directory?("/Applications/Alfred 2.app") }
