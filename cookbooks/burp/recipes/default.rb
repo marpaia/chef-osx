165d99d0eb006826c3d7b349dd6049ff4852b722
Mike Arpaia <mike@arpaia.co>
2013-07-23 01:48:56 -0400

    a more sane way to install burp

--- cookbooks/burp/recipes/default.rb
+++ cookbooks/burp/recipes/default.rb
@@ -1,9 +1,5 @@
-remote_file "#{Chef::Config[:file_cache_path]}/burp.jar" do
+remote_file "#{ENV['HOME']}/Tools/burp.jar" do
   source "http://portswigger.net/burp/burpsuite_free_v1.5.jar"
   checksum "8596fed86278cfa607333358b688a07df4e4c3fc13be42b7ecd6672e4708d641"
-end
-
-execute "move burp to tools" do
-  command "mv #{Chef::Config[:file_cache_path]}/burp.jar ~/Tools/"
   not_if { File.exists?("~/Tools/burp.jar") }
 end
