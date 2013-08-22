e0488189440419960c891779da9c357732646e48
Mike Arpaia <mike@arpaia.co>
2013-08-22 17:40:20 -0400

    goroot

--- cookbooks/zsh/files/default/oh-my-zsh/oh-my-zsh/lib/path.zsh
+++ cookbooks/zsh/files/default/oh-my-zsh/oh-my-zsh/lib/path.zsh
@@ -1,2 +1,3 @@
 export GOPATH=$HOME/go
+export GOROOT=/usr/local/go
 export PATH=/usr/local/Cellar/php54/5.4.15/bin/:/usr/local/share/python:~/Tools/casper/bin:~/pear/bin:/usr/local/mysql/bin:/opt/local/bin:/opt/local/sbin:~/bin:~/github/phantomjs/bin:/opt/chef/bin/:$GOPATH/bin:/usr/local/go/bin/:$PATH
