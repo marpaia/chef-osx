acff0bc5c75ab8944f2e5904a1840a9525b931c0
marpaia <marpaia@etsy.com>
2013-09-10 11:18:29 -0400

    cdpath

--- cookbooks/zsh/files/default/zsh/zsh/lib/path.zsh
+++ cookbooks/zsh/files/default/zsh/zsh/lib/path.zsh
@@ -1,3 +1,4 @@
 export GOPATH=$HOME/go
 export GOROOT=/usr/local/go
 export PATH=/usr/local/Cellar/php54/5.4.15/bin/:/usr/local/share/python:~/Tools/casper/bin:~/pear/bin:/usr/local/mysql/bin:/opt/local/bin:/opt/local/sbin:~/bin:~/github/phantomjs/bin:/opt/chef/bin/:$GOPATH/bin:/usr/local/go/bin/:$PATH
+export CDPATH=.:$HOME:$GOPATH/src/code.google.com/p:$GOPATH/src/github.com:$GOPATH/src/github.com/etsy
