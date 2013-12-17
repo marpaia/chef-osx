8a78194092964916713d4397c10d7cf112d731d0
marpaia <mike@arpaia.co>
2013-12-17 13:47:07 -0800

    gpg

--- cookbooks/installerz/recipes/gpg.rb
+++ cookbooks/installerz/recipes/gpg.rb
@@ -1,5 +1,5 @@
 dmg_package "GPG Suite" do
-  dmg_name "GPG Suite"
+  dmg_name "Install"
   type "pkg"
   source "https://releases.gpgtools.org/GPG%20Suite%20-%202013.10.22.dmg"
   checksum "d37ccf01e5ddd07dd84b76574e99b605ca9ead89cb0c6c126f4045e271eb3841"
