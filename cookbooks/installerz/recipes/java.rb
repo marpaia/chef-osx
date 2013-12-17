563ebbbf1804850420f54909cf5eabbee667f8db
marpaia <mike@arpaia.co>
2013-12-17 13:22:39 -0800

    java

--- cookbooks/installerz/recipes/java.rb
+++ cookbooks/installerz/recipes/java.rb
@@ -1,6 +1,7 @@
 dmg_package "JDK 7 Update 45" do
   source "http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-macosx-x64.dmg"
   checksum "841c4700ca55d199f7ad90fdb487a81ee59d77dabbb14f6e40f9d3bec7a34824"
+  dmg_name "JDK 7 Update 45"
   type "pkg"
   action :install
   only_if {"java -version | grep '1.7'"}
