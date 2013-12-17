6c99fa23297b5af332adfbd2f96fa635b8b30b7f
marpaia <mike@arpaia.co>
2013-12-17 13:48:52 -0800

    gpg wtf

--- cookbooks/installerz/recipes/gpg.rb
+++
@@ -1,8 +0,0 @@
-dmg_package "GPG Suite" do
-  dmg_name "Install"
-  type "pkg"
-  source "https://releases.gpgtools.org/GPG%20Suite%20-%202013.10.22.dmg"
-  checksum "d37ccf01e5ddd07dd84b76574e99b605ca9ead89cb0c6c126f4045e271eb3841"
-  action :install
-  not_if { File.directory?("/Applications/GPG Keychain Access.app") }
-end
