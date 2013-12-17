6cbab32f19658b2a366e8ca1c369ddd031e4f665
marpaia <mike@arpaia.co>
2013-12-17 13:18:20 -0800

    java

--- cookbooks/installerz/recipes/java.rb
+++ cookbooks/installerz/recipes/java.rb
@@ -1,4 +1,4 @@
-dmg "java" do
+dmg_package "java" do
   source "http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-macosx-x64.dmg"
   checksum "841c4700ca55d199f7ad90fdb487a81ee59d77dabbb14f6e40f9d3bec7a34824"
   action :install
