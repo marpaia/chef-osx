b5762a92b8c09f1a64c011f861fd3891aea870d2
marpaia <mike@arpaia.co>
2013-12-17 13:44:20 -0800

    gpg

--- cookbooks/installerz/recipes/gpg.rb
+++ cookbooks/installerz/recipes/gpg.rb
@@ -1,8 +1,8 @@
 dmg_package "GPGTools_Installer" do
   dmg_name "GPGTools_Installer"
   type "pkg"
-  source "https://s3.amazonaws.com/gpgtools/GPGTools-20130330.dmg"
-  checksum "23558e988c1efc38d4a53fa436b58ffe031d60c8868dc44b8061d3b9f2675bc2"
+  source "https://releases.gpgtools.org/GPG%20Suite%20-%202013.10.22.dmg"
+  checksum "d37ccf01e5ddd07dd84b76574e99b605ca9ead89cb0c6c126f4045e271eb3841"
   action :install
   not_if { File.directory?("/Applications/GPG Keychain Access.app") }
 end
