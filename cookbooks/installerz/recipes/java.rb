093891ac879ffad546984963f9a368babc31a00c
marpaia <mike@arpaia.co>
2013-12-17 13:14:33 -0800

    java

---
+++ cookbooks/installerz/recipes/java.rb
@@ -0,0 +1,6 @@
+dmg "java" do
+  source "http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-macosx-x64.dmg"
+  checksum "841c4700ca55d199f7ad90fdb487a81ee59d77dabbb14f6e40f9d3bec7a34824"
+  action :install
+  not_if {"java -version | grep '1.7'"}
+end
