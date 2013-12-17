3b64535ba2652d0eb16dd88419a0e740e7fe3d2d
marpaia <mike@arpaia.co>
2013-12-17 13:32:36 -0800

    java wtf

--- cookbooks/installerz/recipes/java.rb
+++
@@ -1,5 +0,0 @@
-osx_pkg_package "jdk-7u45-macosx-x64" do
-  source "http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-macosx-x64.dmg"
-  checksum "841c4700ca55d199f7ad90fdb487a81ee59d77dabbb14f6e40f9d3bec7a34824"
-  only_if {"java -version | grep '1.7'"}
-end
