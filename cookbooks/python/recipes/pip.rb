a6b29ce5e2e1d02723a0bbe1c7a36ec59db6a870
marpaia <mike@arpaia.co>
2013-12-17 12:45:30 -0800

    pip

--- cookbooks/python/recipes/pip.rb
+++ cookbooks/python/recipes/pip.rb
@@ -2,7 +2,9 @@ users.each do |user|
   if user['python']
     if user['python']['modules']
       user['python']['modules'].each do |mod|
-        python_pip mod
+        python_pip mod do
+          action :install
+        end
       end
     end
   end
